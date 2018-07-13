46ebb8fedd7ea3c3d374c977d4831aae4e550617
jaironsousa1 <jaironsousa@gmail.com>
2018-07-13 11:22:05 -0300

    Ajuste Mapa service

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MapaService} from './mapa.service';
+import { MapaService, Mapa, MapaFiltro } from './mapa.service';
 
 @Component({
   selector: 'app-mapa',
@@ -7,7 +7,7 @@ import { MapaService} from './mapa.service';
   styleUrls: ['./mapa.component.css']
 })
 export class MapaComponent implements OnInit {
-  
+
 
   //Posicao Inicial
   lat: number = -1.455779;
@@ -15,28 +15,29 @@ export class MapaComponent implements OnInit {
   // zoom 
   zoom: number = 14;
 
+  mapa: Mapa;
+
+  mapaFiltro: MapaFiltro;
+
   markers: marker[] = [];
-  nome: any;
+
 
   constructor(
     private mapaService: MapaService
-  ) {
-    this.carregarLabs()
-
-  }
+  ) {}
 
   ngOnInit() {
     //console.log(this.markers);
-    this.findLaboratorio();
+    this.carregarLabs();
 
   }
 
-  carregarLabs(): void {
+  private carregarLabs(): void {
     this.mapaService.findAll()
       .then(labs => {
         console.log(labs);
         labs.laboratorios.forEach(lab => {
-          let maker: marker = { name: lab.nome, sigla: lab.sigla, logradouro: lab.logradouro, cidade: lab.cidadeNome, telefones:lab.telefones[0], website: lab.website, lat: Number(lab.latitude), lng: Number(lab.longitude), draggable: true }
+          let maker: marker = { name: lab.nome, sigla: lab.sigla, logradouro: lab.logradouro, cidade: lab.cidadeNome, telefones: lab.telefones[0], website: lab.website, lat: Number(lab.latitude), lng: Number(lab.longitude), draggable: true }
 
           // this.maker.name = lab.nome;
           // this.maker.lat = lab.latitude;
@@ -47,19 +48,16 @@ export class MapaComponent implements OnInit {
           //maker = null;
         });
 
-
       })
 
     console.log(this.markers);
 
   }
 
-  findLaboratorio() {
-    this.mapaService.findAllParameter({ parametro: this.nome })
-      .then((findLaboratorio) => this.findLaboratorio = findLaboratorio);
+  public findLaboratorio() {
+    this.mapaService.findAllParameter(this.mapaFiltro)
+      .then((mapa) => this.mapa = mapa);
   }
-
-
 
 }
 
--- src/app/mapa/mapa.service.ts
+++ src/app/mapa/mapa.service.ts
@@ -9,51 +9,58 @@ export interface MapaFiltro {
   parametro: string;
 }
 
+export interface Mapa {
+  laboratorios: any,
+  servicos: any,
+  instituicoes: any,
+  pesquisadores: any,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class MapaService {
-  
+
   private resourceUrl;
 
   constructor(private http: Http) {
     this.resourceUrl = `${environment.baseUrl}/api/mapa`
-   }
+  }
 
   findAll(): Promise<any> {
     return this.http.get(`${this.resourceUrl}/laboratorios`)
       .toPromise()
       .then(res => res.json() as any)
-      //.catch(this.handleError);
+    //.catch(this.handleError);
   }
 
   //Busca de laboratorios
-  findAllParameter(filtro: MapaFiltro):Promise<any>{
+  findAllParameter(filtro: MapaFiltro): Promise<any> {
 
     const params = new URLSearchParams();
     // const headers = new Headers();
 
     //headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
-    
+
     if (filtro.parametro) {
-      params.set('nome', filtro.parametro);
+      params.set('parametro', filtro.parametro);
     }
 
     return this.http.get(`${this.resourceUrl}`,
-    { search: params })
-    .toPromise()
-    .then(res => res.json() as any)
+      { search: params })
+      .toPromise()
+      .then(res => res.json() as Mapa)
   }
-  
-  }
-  
+
+}
+
 export class Laboratorio {
   nome: string;
   sigla: string;
   logradouro: string;
   cidadeNome: string;
   telefones: string;
-  website:string;
+  website: string;
   latitude: number;
   longitude: number;
 }
@@ -61,11 +68,11 @@ export class Laboratorio {
 export class Instituicao {
   nome: string;
   sigla: string;
-  website:string;
-  descricao:string;
-  status:string;
+  website: string;
+  descricao: string;
+  status: string;
   cidadeNome: string;
-  
+
 }
 
 export class Servicos {
@@ -74,8 +81,8 @@ export class Servicos {
   status: string;
   tipoServico: string;
   laboratorio: string;
-  setoresEconomicos:string;
-  
+  setoresEconomicos: string;
+
 }
 
 export class Pesquisadores {
@@ -84,7 +91,7 @@ export class Pesquisadores {
   idCNPQ: string;
   dataSincronizacao: string;
   titulacao: string;
-  atuacao:string;
+  atuacao: string;
   user: string;
-  
+
 }
