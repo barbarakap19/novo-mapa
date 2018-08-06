97a34ce341bac202c1a492fab9aadf84a9fe018e
jaironsousa1 <jaironsousa@gmail.com>
2018-08-06 09:21:55 -0300

    Ajuste da chave API_KEY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ import { ContatoComponent } from './contato/contato.component';
     NgbModule.forRoot(),
     ToastyModule.forRoot(),
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAmWYmdS7pWTwJ18E9gX60I0c2D6mgd7xY'
+      apiKey: 'AIzaSyAIpSn_dwDfhsAKd1ksVSRXWEq-eEdrowg'
     }),
     AgmJsMarkerClustererModule
   ],
