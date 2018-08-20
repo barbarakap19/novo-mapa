fc9864c7cafee3888c2d5959912576f0227b8894
jaironsousa1 <jaironsousa@gmail.com>
2018-08-20 15:37:28 -0300

    Ajuste envioment.ts para url prodepa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  /* baseUrl: 'http://177.74.1.48:8080', */
-  baseUrl: 'http://104.237.139.21:8080',
+  baseUrl: 'http://177.74.1.48:8080',
+ /*  baseUrl: 'http://104.237.139.21:8080', */
   baseIconnectUrl: 'http://iconnect.pctguama.org.br/iconect/v1',
   baseIconnectUrlLocal: 'assets/json/iconnect_mapa.json'
 };
